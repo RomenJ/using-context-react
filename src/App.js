babab51b78a323af54b0e7437d867fa4dabf7dd6
RomenJ <justiciaterapeutica@gmail.com>
2023-09-07 19:10:31 +0200

    adding exta extra functions

--- src/App.js
+++ src/App.js
@@ -6,65 +6,100 @@ import Background from './components/Background'
 import Container from './components/Container'
 import HeaderApp from './components/HeaderApp'
 
+const Context = createContext();
 
-const Context = createContext()
-/**Context se usará generalmente para variables que no cambian */
-const ContadorProvider = ({children}) => {
-    const [contador, setCont] =useState(0)
-    const incrementar =useCallback(()=> setCont (x =>x+1), [])
-    const decrementar = useCallback(() => setCont (x=> x-1, []))
-    return(
-        <Context.Provider value={{contador, incrementar, decrementar}}>
-            {children}
-        </Context.Provider>
-    )
+const ContadorProvider = ({ children }) => {
+  const [contador, setCont] = useState(0);
+  const [ultimaAccion, setUltimaAccion] = useState(null);
+  const [ultimaFecha, setUltimaFecha] = useState(null);
+
+  const incrementar = useCallback(() => {
+    setCont(x => x + 1);
+    setUltimaAccion("Incrementar (+)");
+    setUltimaFecha(new Date().toLocaleString());
+  }, []);
+
+  const decrementar = useCallback(() => {
+    setCont(x => x - 1);
+    setUltimaAccion("Decrementar (-)");
+    setUltimaFecha(new Date().toLocaleString());
+  }, []);
+
+  return (
+    <Context.Provider value={{ contador, incrementar, decrementar, ultimaAccion, ultimaFecha }}>
+      {children}
+    </Context.Provider>
+  );
 }
 
-const Incrementar =memo(() =>{
-    console.log ('incrementar')
-    const {incrementar} = useContext (Context)
-    return(
-        <button onClick= {incrementar}> Incrementar (+)</button>
-    )
+const Incrementar = memo(() => {
+  console.log('incrementar');
+  const { incrementar } = useContext(Context);
+  return (
+    <button onClick={incrementar}>Incrementar (+)</button>
+  );
+});
 
-})
-
-const Decrementar = memo(()=>{
-    console.log ('decrementar')
-     const {decrementar}= useContext(Context)
-     return(
-        <button onClick={decrementar}> Decrementar (-)</button>
-     )
-})
+const Decrementar = memo(() => {
+  console.log('decrementar');
+  const { decrementar } = useContext(Context);
+  return (
+    <button onClick={decrementar}>Decrementar (-)</button>
+  );
+});
 
 
-const Label =() =>{
+const Label = () => {
+  console.log('label render');
+  const { contador, ultimaAccion, ultimaFecha } = useContext(Context);
+  
+const valores = [contador, ultimaAccion, ultimaFecha];
 
-        console.log ('label render')
-        const {contador} = useContext(Context)
-        return (
+return (
+  <div>
+    <h1>{contador}</h1>
+    
+    {/* Mostrar los valores al final de Label */}
+    <div>
+      <p>Valor: {contador}</p>
+      <p>Última acción: {ultimaAccion}</p>
+      <p>Fecha: {ultimaFecha}</p>
+    </div>
+    
+  </div>
 
-            <h1> {contador}</h1>
-        )       
-
+  
+);
 }
 
-const App = ()=>{
-    return(
-        <Background>
-            <HeaderApp> Aumentador y reductor en React </HeaderApp>
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            </header>
-            <Container>
-                <ContadorProvider>
-                    <Label /> 
-                    <Incrementar />
-                    <Decrementar/>
-                </ContadorProvider>
-        </Container>
-        </Background>
-    )
 
+const App = () => {
+  return (
+    <Background>
+      <HeaderApp> Aumentador y reductor en React </HeaderApp>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Container>
+        <ContadorProvider>
+        <table>
+        <tr>
+        <td> <Incrementar /></td>
+        <td> <Decrementar /></td>
+        </tr>
+        <tr>
+          <td> <Label /> </td>
+          </tr>
+        </table>
+        </ContadorProvider>
+      </Container>
+    </Background>
+  );
 }
-export default App
+
+export default App;
+
+
+
+
+
